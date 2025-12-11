f99234761ee9f206f9d1a3c081b8272e1fe539bd
Vamsikrishna3727 <nvamsi.krishna2001@gmail.com>
2025-12-11 14:40:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css'; // ← your external CSS
+import './style.css'; // ← your external CSS
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
